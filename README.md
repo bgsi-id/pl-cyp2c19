74ec588a5073302d4abbcf4ba39db5f697eb6206
satriobio <satrio.biology@yahoo.com>
2024-01-25 00:00:22 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# pl-cyp2c19
+## Introduction
+Align nanopore reads to reference (CYP2C19 gene, form genome build 38), variant calling using Clair3, and adjust to make it compatible with Pharmcat, check for missing important sites in CYP2C19.
+
+## Installation
+Install Dependencies
+```
+conda config --add channels defaults && \
+conda config --add channels bioconda && \
+conda config --add channels conda-forge && \
+conda create -n pl-cyp2c19 -c bioconda clair3 samtools minimap2 python=3.9.0 -y
+conda activate pl-cyp2c19
+```
+
+Download Reference
+```
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+gunzip -d GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+samtools faidx GCA_000001405.15_GRCh38_no_alt_analysis_set.fna chr10:94762681-94855547 > GRCh38.cyp2c19.fa 
+samtools faidx GRCh38.cyp2c19.fa
+```
+
+Download Clair3 Model
+```
+## see 'https://github.com/nanoporetech/rerio/tree/master/clair3_models' for full model
+wget https://cdn.oxfordnanoportal.com/software/analysis/models/clair3/r1041_e82_400bps_sup_v410.tar.gz
+tar -xvf r1041_e82_400bps_sup_v410.tar.gz
+```
+## Usage
+Run Pipeline
+```
+bash run.sh \
+    -s sample \
+    -f /dir/to/fastq \
+    -r /reference.fa \
+    -t 48 \
+    -m /dir/to/model \
+    -o /dir/output
+
+## example
+# bash run.sh \
+#     -s barcode06 \
+#     -f /data/rspon/Batch_1/fastq_pass/barcode06 \
+#     -r /data/refs/GRCh38.cyp2c19.fa \
+#     -t 48 \
+#     -m /data/models/r1041_e82_400bps_sup_v410 \
+#     -o output-barcode06
+```
+
+Run Pharmcat
+```
+docker run -it -v `pwd`:/data pgkb/pharmcat /pharmcat/pharmcat_pipeline /data/sample.hg38.vcf -o /data/pcat
+
+## example
+# docker run -it -v `pwd`:/data pgkb/pharmcat /pharmcat/pharmcat_pipeline /data/output-barcode06/barcode06.hg38.vcf -o /data/pcat
+```
+
+
+
---
+++ run.sh
@@ -0,0 +1,144 @@
+while getopts ":s:t:f:r:m:o:" opt; do
+  case $opt in
+    s)
+      SAMPLE=$OPTARG
+      ;;
+    t)
+      T=$OPTARG
+      ;;
+    f)
+      FASTQ=$OPTARG
+      ;;
+    r)
+      REF=$OPTARG
+      ;;
+    m)
+      MODEL=$OPTARG
+      ;;
+    o)
+      OUTPUT=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo "Checking environment ..."
+required_tools=("minimap2" "samtools" "run_clair3.sh")
+
+for tool in "${required_tools[@]}"; do
+    which "$tool" > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "- $tool is already installed."
+    else
+        echo "- $tool is not installed."
+        exit
+    fi
+done
+
+mkdir $OUTPUT
+
+# Step 1
+echo "(1/4) Mapping reads to reference"
+zcat $FASTQ/*.fastq.gz | minimap2 -ax map-ont $REF - | samtools sort - > $OUTPUT/$SAMPLE.bam && samtools index $OUTPUT/$SAMPLE.bam
+
+# Check the exit status of the previous command
+if [ $? -ne 0 ]; then
+    echo "Error in mapping reads to reference. Exiting."
+    exit 1
+fi
+
+# Step 2
+echo "(2/4) Variant calling"
+run_clair3.sh \
+  --bam_fn=$OUTPUT/$SAMPLE.bam \
+  --ref_fn=$REF \
+  --platform="ont" \
+  --threads=$T \
+  --model_path=$MODEL \
+  --include_all_ctgs \
+  --var_pct_full=1 \
+  --var_pct_phasing=1 \
+  --gvcf \
+  --output=$OUTPUT/clair3
+
+# Check the exit status of the previous command
+if [ $? -ne 0 ]; then
+    echo "Error in variant calling. Exiting."
+    exit 1
+fi
+
+# Step 3
+echo "(3/4) Liftover to hg38"
+zcat clair3/merge_output.vcf.gz | awk 'BEGIN{OFS="\t"} {if ($1=="chr10:94762681-94855547") {$1="chr10"; $2+=94762680} print}' - > $OUTPUT/$SAMPLE.hg38.vcf
+zcat clair3/merge_output.gvcf.gz | awk 'BEGIN{OFS="\t"} {if ($1=="chr10:94762681-94855547") {$1="chr10"; $2+=94762680} print}' - > $OUTPUT/$SAMPLE.hg38.gvcf
+
+# Check the exit status of the previous command
+if [ $? -ne 0 ]; then
+    echo "Error in liftover to hg38. Exiting."
+    exit 1
+fi
+
+# Step 4
+echo "(4/4) Checking variants"
+echo "CHR\tPOS\tDP" > $OUTPUT/$SAMPLE.depth.txt
+# Loop through the provided positions
+for position in \
+  "94781859" \
+  "94761900" \
+  "94762706" \
+  "94762712" \
+  "94762715" \
+  "94762755" \
+  "94762760" \
+  "94762788" \
+  "94762856" \
+  "94775106" \
+  "94775121" \
+  "94775160" \
+  "94775185" \
+  "94775367" \
+  "94775416" \
+  "94775423" \
+  "94775453" \
+  "94775489" \
+  "94775507" \
+  "94780574" \
+  "94780579" \
+  "94780653" \
+  "94781858" \
+  "94781944" \
+  "94781999" \
+  "94842861" \
+  "94842866" \
+  "94842879" \
+  "94842995" \
+  "94849995" \
+  "94852738" \
+  "94852765" \
+  "94852785" \
+  "94852914"
+
+do
+  # Extract depth from VCF for the given position
+  depth=$(grep $position $OUTPUT/$SAMPLE.hg38.gvcf | awk '{print $10}' | awk -F':' '{print $3}')
+  #Print result
+  if [ -n "$depth" ]; then
+    echo -e "chr10\t$position\t$depth" >> $OUTPUT/$SAMPLE.depth.txt
+  else
+    echo -e "chr10\t$position\tNo depth" >> $OUTPUT/$SAMPLE.depth.txt
+  fi
+done
+
+if [ $? -ne 0 ]; then
+    echo "Error in liftover to hg38. Exiting."
+    exit 1
+fi
+
+echo "Complete!"
